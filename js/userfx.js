d3b909e42417647196804542a82418138763824a
Sean Ryan <seanpryan89@gmail.com>
2019-02-17 12:53:12 -0500

    added onScroll nav underlines

--- js/userfx.js
+++ js/userfx.js
@@ -1,30 +1,77 @@
-var seanRyan = document.querySelector("#sean-ryan");
-var about = document.querySelector("#about-link");
-var projects = document.querySelector("#projects-link");
-var contact = document.querySelector("#contact-link");
-
 var navItems = document.querySelectorAll(".nav-item")
+var activeNavItem = null;
+//individual nav items
+var seanRyan = document.querySelector("#sean-ryan");
+var home = document.querySelector("#home");
+var projects = document.querySelector("#projects");
+var about = document.querySelector("#about");
+var contacat = document.querySelector("#contact");
 
+//add and blue underline to or from "Sean Ryan" text
+const addUnderlineToSr = () => {
+  seanRyan.classList.add("text-highlight-blue");
+  activeNavItem = seanRyan;
+};
 
-var active = seanRyan;
-var prev = null;
+const removeUnderlineFromSr = () => {
+  seanRyan.classList.remove("text-highlight-blue");
+};
 
+//add or remove blue underline to or from nav item
+const addUnderlineToNavItem = (item) => {
+  item.classList.add("text-highlight-blue-li");
+  activeNavItem = item;
+}
+
+const removeUnderlineFromNavItem = (item) => {
+  item.classList.remove("text-highlight-blue-li");
+}
+
+//listen for nav bar item click
 document.addEventListener("click", function(e) {
-  // console.log(e.target);
   if (e.target.matches(".nav-item")){
-    navItems.forEach(function(element, index){
-      console.log(element);
+    navItems.forEach(function(element){
       if (element.matches("#sean-ryan")){
-        element.classList.remove("text-highlight-blue")
+        removeUnderlineFromSr(e.target);
       } else {
-        element.classList.remove("text-highlight-blue-li");
+        removeUnderlineFromNavItem(element);
       }
-      console.log(e.target)
       if (e.target.matches("#sean-ryan")){
-        e.target.classList.add("text-highlight-blue");
+        addUnderlineToSr(e.target);
       } else {
-        e.target.classList.add("text-highlight-blue-li");
+        addUnderlineToNavItem(e.target);
       }
     })
     }
   }, false)
+
+const addUnderlineOnScroll = (active, scrollPos) => {
+  removeUnderlineFromSr();
+  navItems.forEach((element) => {
+    removeUnderlineFromNavItem(element);
+    if (scrollPos <= 316){
+      addUnderlineToSr();
+    } else if (element = active){
+      addUnderlineToNavItem(active);
+    }
+  });
+};
+
+//add blue underline to nav items on scroll
+window.addEventListener("scroll", function(e){
+  let scrollPos = pageYOffset;
+  if (scrollPos <= 315){
+    activeNavItem = home;
+    console.log(activeNavItem)
+    addUnderlineOnScroll(activeNavItem, scrollPos);
+  } else if (scrollPos >= 316 && scrollPos <= 1233){
+    activeNavItem = projects;
+    addUnderlineOnScroll(activeNavItem, scrollPos);
+  } else if (scrollPos >= 1234 && scrollPos <= 1937) {
+    activeNavItem = about;
+    addUnderlineOnScroll(activeNavItem, scrollPos);
+  } else if (scrollPos >= 1938) {
+    activeNavItem = contact;
+    addUnderlineOnScroll(activeNavItem, scrollPos);
+  }
+})
