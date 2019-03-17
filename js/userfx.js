42a4f3fcc6de6fe3c0825a1adc050d6693eaefb8
Sean Ryan <seanpryan89@gmail.com>
2019-03-17 15:11:25 -0400

    made mobile nav clickable

--- js/userfx.js
+++ js/userfx.js
@@ -82,10 +82,10 @@ window.addEventListener("scroll", function(e) {
 //increase/decrease height of mobile nav container on hamburger click
 document.addEventListener("click", function(e) {
     var mobileNavBackground = document.querySelector(".navbar-wrapper");
-      if (e.target.matches(".fas") && fullScreenNav === false){
+      if (e.target.matches(".custom-nav-toggle") && fullScreenNav === false){
         mobileNavBackground.style.height = "100vh";
         fullScreenNav = true;
-      } else if (e.target.matches(".fas") || e.target.matches(".nav-item") || fullScreenNav === true){
+      } else if (e.target.matches(".custom-nav-toggle") || e.target.matches(".nav-item") || fullScreenNav === true){
         mobileNavBackground.style.height = "100px";
         fullScreenNav = false;
       }
