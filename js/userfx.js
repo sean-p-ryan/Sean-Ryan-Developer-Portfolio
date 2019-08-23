ad1f084f580773be567da896cdd32e1b17254e57
Sean Ryan <seanpryan89@gmail.com>
2019-08-22 20:28:39 -0400

    fixed projects link bug

--- js/userfx.js
+++ js/userfx.js
@@ -1,6 +1,3 @@
-//checks whether nav bar background is full screen
-let fullScreenNav = false;
-
 //selects "Sean Ryan" text in  and "Projects" in nav bar
 let seanRyan = document.querySelector("#sean-ryan");
 let projects = document.querySelector("#projects");
@@ -20,93 +17,85 @@ let scrollPos = pageYOffset;
 
 //add underline to "Sean Ryan" text
 const addUnderlineToSr = () => {
-  seanRyan.classList.add("text-highlight-blue");
+    seanRyan.classList.add("text-highlight-blue");
 };
 
 //remove underline from "Sean Ryan" text
 const removeUnderlineFromSr = () => {
-  seanRyan.classList.remove("text-highlight-blue");
+    seanRyan.classList.remove("text-highlight-blue");
 };
 
 //remove blue underline from "projects"
 const removeUnderlineFromProjects = () => {
-  projects.classList.remove("text-highlight-blue-li");
+    projects.classList.remove("text-highlight-blue-li");
 };
 
 //add blue underline to "projects"
 const addUnderlineToProjects = () => {
-  projects.classList.add("text-highlight-blue-li");
+    projects.classList.add("text-highlight-blue-li");
 };
 
 // add gray background to nav on scroll
 const addGrayNavBackground = (scrollPos) => {
-  if (scrollPos > 0) {
-    navBar.classList.add("nav-background-gray");
-    navBar.classList.remove("nav-background-none");
-  } else {
-    navBar.classList.add("nav-background-none");
-  }
+    if (scrollPos > 0) {
+        navBar.classList.add("nav-background-gray");
+        navBar.classList.remove("nav-background-none");
+    } else {
+        navBar.classList.add("nav-background-none");
+    }
 }
 
 //expose mobile nav options on icon click
 const mobileNavToggle = (e) => {
-  console.log("I was clicked")
-  if (fullScreenNav === false) {
-    console.log("I was clicked too")
-    navBar.style.height = "100vh";
-    fullScreenNav = true;
-  } else if (fullScreenNav === true) {
     navBar.style.height = "100px";
-    fullScreenNav = false;
-  }
 }
 
 
 // listen for click on nav items and toggle icon
 document.addEventListener(
-  "click",
-  function (e) {
-    console.log(e.target);
-    if (e.target.matches("#projects")) {
-      removeUnderlineFromSr();
-      addUnderlineToProjects();
-      mobileNavToggle(e);
-    } else if (e.target.matches("#sean-ryan")) {
-      removeUnderlineFromProjects();
-      addUnderlineToSr();
-    }
-    if (e.target.closest(".custom-nav-toggle")) {
-      mobileNavToggle(e);
-    }
-  });
+    "click",
+    function(e) {
+        console.log(e.target);
+        if (e.target.matches("#projects")) {
+            removeUnderlineFromSr();
+            addUnderlineToProjects();
+            mobileNavToggle(e);
+        } else if (e.target.matches("#sean-ryan")) {
+            removeUnderlineFromProjects();
+            addUnderlineToSr();
+        }
+        if (e.target.closest(".custom-nav-toggle")) {
+            mobileNavToggle(e);
+        }
+    });
 
 
-window.addEventListener("load", function () {
-  let indexRegExp = new RegExp("index");
-  if (indexRegExp.test(window.location.href) && (window.pageYOffset != 0)) {
-    removeUnderlineFromSr();
-    addUnderlineToProjects();
-  } else if (indexRegExp.test(window.location.href)) {
-    addUnderlineToSr();
-  };
+window.addEventListener("load", function() {
+    let indexRegExp = new RegExp("index");
+    if (indexRegExp.test(window.location.href) && (window.pageYOffset != 0)) {
+        removeUnderlineFromSr();
+        addUnderlineToProjects();
+    } else if (indexRegExp.test(window.location.href)) {
+        addUnderlineToSr();
+    };
 })
 
 //change nav underline on scroll
 const changeUnderlineOnScroll = (scrollPos) => {
-  if (scrollPos >= 770) {
-    removeUnderlineFromSr();
-    addUnderlineToProjects();
-  } else {
-    removeUnderlineFromProjects();
-    addUnderlineToSr();
-  }
+    if (scrollPos >= 770) {
+        removeUnderlineFromSr();
+        addUnderlineToProjects();
+    } else {
+        removeUnderlineFromProjects();
+        addUnderlineToSr();
+    }
 };
 
 // change nav bar height on scroll
-window.addEventListener("scroll", function (e) {
-  scrollPos = pageYOffset;
-  changeUnderlineOnScroll(scrollPos);
-  if (window.innerWidth > 768) {
-    addGrayNavBackground(scrollPos)
-  }
+window.addEventListener("scroll", function(e) {
+    scrollPos = pageYOffset;
+    changeUnderlineOnScroll(scrollPos);
+    if (window.innerWidth > 768) {
+        addGrayNavBackground(scrollPos)
+    }
 });
